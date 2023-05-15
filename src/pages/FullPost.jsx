217f7bc1130a6f2046f1d26f48b08dc8043d9ec8
Vladimir <homie43@yandex.ru>
2023-05-15 17:33:36 +0300

    fixed undefined id again

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -23,11 +23,9 @@ export const FullPost = () => {
         isFullPost
       >
         <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
+          Hey there! 👋 I'm starting a new series called "Roast the Code", where I will share some code, and let YOU
+          roast and improve it. There's not much more to it, just be polite and constructive, this is an exercise so we
+          can all learn together. Now then, head over to the repo and roast as hard as you can!!
         </p>
       </Post>
       <CommentsBlock
