0a0ebf0ea6215004b1be2a603517b0280af3ea34
Vladimir <homie43@yandex.ru>
2023-05-18 18:53:44 +0300

    completed valid form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,25 +4,57 @@ import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
+import { useForm } from "react-hook-form";
+
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  console.log(errors, isValid);
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register("email", { required: "Укажите почту" })}
+          type="email"
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register("password", { required: "Укажите пароль" })}
+          type="password"
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
