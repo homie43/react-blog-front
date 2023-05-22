a6d6f136ec4857b31ecba6cd07ece82811115cab
Vladimir <homie43@yandex.ru>
2023-05-22 12:40:57 +0300

    finished registration form

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -11,6 +11,11 @@ export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
   return data;
 });
 
+export const fetchRegister = createAsyncThunk("auth/fetchRegister", async (params) => {
+  const { data } = await axios.post("auth/register", params);
+  return data;
+});
+
 const initialState = {
   data: null,
   status: "loading",
@@ -48,6 +53,19 @@ const authSlice = createSlice({
       state.data = action.payload;
     },
     [fetchAuthMe.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
+
+    [fetchRegister.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+    [fetchRegister.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchRegister.rejected]: (state) => {
       state.status = "error";
       state.data = null;
     },
